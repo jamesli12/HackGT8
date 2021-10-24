6435315a61218905696cf4838d74f315459e8f1b
jamesli12 <leej55064@gmail.com>
2021-10-24 00:24:50 -0400

    Final?

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ async function main() {
     var collection = db.collection('posts');
 
     app.get('/', (req, res) => {
-        res.render('index.pug', {});
+        res.render('index.pug', {}); //redirect to the index page
     })
 
     var upload = multer(); //parsing use
@@ -43,52 +43,52 @@ async function main() {
         })
     });
 
-    app.get('/index', function (req, res) {
+    app.get('/index', function (req, res) { //redirect to index
         res.render('index.pug');
     })
     
-    app.get('/resources', function (req, res) {
+    app.get('/resources', function (req, res) { //redirect to resources 
         res.render('resources.pug');
     })
 
-    app.get('/search', function (req, res) {
+    app.get('/search', function (req, res) { //search bar request
         var searchText = req.query.searchText.toLowerCase();
-        collection.find({}).toArray(function (err, result) {
+        collection.find({}).toArray(function (err, result) { //get all posts from database
             if (err) {
                 res.send(err);
             } else {
                 var index = 0;
                 var store = [];
                 for (var x = 0; x < result.length; x++) {
-                    if (result[x].ident.toLowerCase().includes(searchText) || result[x].rant.toLowerCase().includes(searchText)) {
+                    if (result[x].ident.toLowerCase().includes(searchText) || result[x].rant.toLowerCase().includes(searchText)) { //lowercase comparison for the search
                         store[index] = result[x];
-                        if (store[index].isAnonymous) {
+                        if (store[index].isAnonymous) { //replace name for anonymous user
                             store[index].fname = "Anonymous User";
                         }
-                        index++;
+                        index++; //this index makes sure that there's no null spots in store
                     }
                 }
                 res.render('about.pug', {
-                    lines: store
+                    lines: store //render with entries
                 });
             }
         })
     })
 
-    app.get('/about', function (req, res) {
-        collection.find({}).toArray(function (err, result) {
+    app.get('/about', function (req, res) { //redirect to about page
+        collection.find({}).toArray(function (err, result) { //get all posts from database
             if (err) {
                 res.send(err);
             } else {
                 var store = [];
                 for (var x = 0; x < result.length; x++) {
                     store[x] = result[x];
-                    if (store[x].isAnonymous) {
+                    if (store[x].isAnonymous) { //replace name for anonymous user
                         store[x].fname = "Anonymous User";
                     }
                 }
                 res.render('about.pug', {
-                    lines: store
+                    lines: store //render with entries
                 });
             }
         })
